ff222618cc62b26969e250167eb1903df639ba49
amankumar <amanjane15@gmail.com>
2026-04-13 02:44:10 +0530

    Fix catch-all route to serve index.html correctly
    Refactor catch-all route to properly serve index.html for non-file requests.

--- server.js
+++ server.js
@@ -20,11 +20,15 @@ app.use('/subscribe', require('./routes/subscribe'));
 app.use('/admin', require('./routes/admin'));
 
 /* ── Catch-all (IMPORTANT FIX) ── */
-app.get('*', (req, res, next) => {
+/*app.get('*', (req, res, next) => {
   // If it's a file request (.js, .css, .png, etc), don't send index.html
   if (path.extname(req.path)) {
     return next();
-  }
+  }*/
+app.get('*', (req, res, next) => {
+  if (path.extname(req.path)) return next();
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
   res.sendFile(path.join(__dirname, 'index.html'));
 });
